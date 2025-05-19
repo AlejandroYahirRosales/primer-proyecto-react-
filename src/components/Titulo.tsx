339941962b7b92c786f334f2363f930e57507e80
Raulez3101 <cbbe.raulomar.enriquez.zu@unifranz.edu.bo>
2025-05-19 19:48:46 -0400

    segundo_commit

---
+++ src/components/Titulo.tsx
@@ -0,0 +1,10 @@
+function Titulo(){
+    return (
+        <div className="App">
+          <h1>Bienvenido a React</h1>
+          <p>Desde Titulo <code>src/App.tsx</code>and save the reload.</p>
+        </div>
+      );
+}
+
+export default Titulo;
